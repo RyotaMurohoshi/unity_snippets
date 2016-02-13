09f0ba078ebaca8244399348b9e3dd5e4502c95b
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-02-13 16:19:21 +0900

    added yield instruction

---
+++ unity/Assets/CustomYieldInstructionExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomYieldInstructionExample : MonoBehaviour
+{
+	[SerializeField] bool waitUntil = false;
+	[SerializeField] bool waitWhile = true;
+
+	void Start ()
+	{
+		StartCoroutine (WaitUntilCoroutine ());
+		StartCoroutine (WaitWhileCoroutine ());
+	}
+
+	IEnumerator WaitUntilCoroutine ()
+	{
+		Debug.Log ("WaitUntilCoroutine Begin");
+		yield return new WaitUntil (() => waitUntil);
+		Debug.Log ("WaitUntilCoroutine Finish");
+	}
+
+	IEnumerator WaitWhileCoroutine ()
+	{
+		Debug.Log ("WaitWhileCoroutine Begin");
+		yield return new WaitWhile (() => waitWhile);
+		Debug.Log ("WaitWhileCoroutine Finish");
+	}
+}
