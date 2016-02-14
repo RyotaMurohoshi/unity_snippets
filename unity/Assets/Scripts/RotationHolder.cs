ff9951b61f30c0149b4310507048487e5136c1af
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-14 15:41:13 +0900

    added RotationHolder

---
+++ unity/Assets/Scripts/RotationHolder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotationHolder : MonoBehaviour
+{
+    Quaternion rotation;
+
+    void Awake()
+    {
+        rotation = transform.rotation;            
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
