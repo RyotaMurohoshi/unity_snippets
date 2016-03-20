7ca5cb9ac13f23619c395620588bf5b9880144f0
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-20 16:05:57 +0900

    created MyWaitForSeconds

--- unity/Assets/Scripts/Examples/CustomYieldInstructionExample.cs
+++ unity/Assets/Scripts/Examples/CustomYieldInstructionExample.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CustomYieldInstructionExample : MonoBehaviour
 {
@@ -12,7 +13,11 @@ public class CustomYieldInstructionExample : MonoBehaviour
     {
         StartCoroutine(WaitUntilCoroutine());
         StartCoroutine(WaitWhileCoroutine());
+<<<<<<< 4c8db37d471b502ea05afde7ed54f4f7bcc2d007
         StartCoroutine(MyWaitForSecondsCoroutine());
+=======
+        StartCoroutine(RepeatMyWaitForSecondsCoroutine(count: 3, seconds:1.0F));
+>>>>>>> created MyWaitForSeconds
     }
 
     IEnumerator WaitUntilCoroutine()
@@ -29,10 +34,27 @@ public class CustomYieldInstructionExample : MonoBehaviour
         Debug.Log("WaitWhileCoroutine Finish");
     }
 
+<<<<<<< 4c8db37d471b502ea05afde7ed54f4f7bcc2d007
     IEnumerator MyWaitForSecondsCoroutine()
     {
         Debug.Log("MyWaitForSecondsCoroutine Begin : " + System.DateTime.Now);
         yield return new MyWaitForSeconds(5.0F);
         Debug.Log("MyWaitForSecondsCoroutine Finish : " + System.DateTime.Now);
     }
+=======
+    IEnumerator MyWaitForSecondsCoroutine(float seconds)
+    {
+        Debug.Log("MyWaitForSecondsCoroutine Begin : " + System.DateTime.Now);
+        yield return new MyWaitForSeconds(seconds);
+        Debug.Log("MyWaitForSecondsCoroutine Finish : " + System.DateTime.Now);
+    }
+
+    IEnumerator RepeatMyWaitForSecondsCoroutine(int count, float seconds)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            yield return MyWaitForSecondsCoroutine(seconds);
+        }
+    }
+>>>>>>> created MyWaitForSeconds
 }
