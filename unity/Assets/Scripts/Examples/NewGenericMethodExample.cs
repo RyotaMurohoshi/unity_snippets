dbbcd878e1d8deee5a9285b356ec0dfa43f4f638
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-21 12:17:38 +0900

    added unity snipetts

---
+++ unity/Assets/Scripts/Examples/NewGenericMethodExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewGenericMethodExample : MonoBehaviour
+{
+    [SerializeField]
+    Enemy enemy;
+
+    void Start()
+    {
+        var enemyPrefab = Resources.Load<Enemy>("Prefabs/Enemy");
+        var enemy = Instantiate(enemyPrefab);
+    }
+}
