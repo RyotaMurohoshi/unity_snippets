36888931c503b196d2b32e9ba043b6df7b79da7f
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-03 23:39:24 +0900

    created CreateAssetMenu

---
+++ unity/Assets/Scripts/Examples/EnemytData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField]
+    int level;
+    [SerializeField]
+    string playerName;
+
+    public int Level { get { return level; } }
+    public string PlayerName { get { return playerName; } }
+}
