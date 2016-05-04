13ff72dbbe46c1f1f9b9dec9129da4449f58cd07
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-04 23:27:31 +0900

    created CoroutineWHenAll

---
+++ unity/Assets/Scripts/Examples/CoroutineWhenAll.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class CoroutineWhenAll : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        var list = new[] {
+            new {
+                delaySeconds = 1.0F,
+                message = "Hello"
+            },
+            new {
+                delaySeconds = 3.0F,
+                message = "Bye"
+            },
+            new {
+                delaySeconds = 5.0F,
+                message = "Good night"
+            }
+        };
+
+        yield return Observable
+            .WhenAll(
+                list
+                    .ToObservable()
+                    .SelectMany(it => Observable.FromCoroutine(() => ShowAfterDelay(it.message, it.delaySeconds)))
+            ).StartAsCoroutine();
+
+        Debug.Log("Finished");
+    }
+
+
+    IEnumerator ShowAfterDelay(string message, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        Debug.Log(message);
+    }
+}
