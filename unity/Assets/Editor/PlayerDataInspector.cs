c9adccdc7ecef86731ca2143f2ac17ac139339ca
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-05 23:07:11 +0900

    show Sprite

--- unity/Assets/Editor/PlayerDataInspector.cs
+++ unity/Assets/Editor/PlayerDataInspector.cs
@@ -12,9 +12,14 @@ public class PlayerDataInspector : Editor
         serializedObject.ApplyModifiedProperties();
     }
 
+    //各要素の描画
     void DrawProperties()
     {
+        PlayerData obj = target as PlayerData;
+
         EditorGUILayout.PropertyField(serializedObject.FindProperty("playerName"), new GUIContent("PlayerName"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty("level"), new GUIContent("Level"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("sprite"), new GUIContent("Sprite"));
+        GUILayout.Label(obj.Sprite.texture);
     }
 }
--- unity/Assets/Scripts/Examples/PlayerData.cs
+++ unity/Assets/Scripts/Examples/PlayerData.cs
@@ -7,7 +7,10 @@ public class PlayerData : ScriptableObject
     int level;
     [SerializeField]
     string playerName;
+    [SerializeField]
+    Sprite sprite;
 
     public int Level { get { return level; } }
     public string PlayerName { get { return playerName; } }
+    public Sprite Sprite { get { return sprite; } }
 }
