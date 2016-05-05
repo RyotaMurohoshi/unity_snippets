849a88021e4d35933117f98c26fd155e2830357d
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-05 21:54:48 +0900

    added CustomEditor

---
+++ unity/Assets/Editor/PlayerDataInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(PlayerData))]
+public class PlayerDataInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        DrawProperties();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void DrawProperties()
+    {
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("playerName"), new GUIContent("PlayerName"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("level"), new GUIContent("Level"));
+    }
+}
