1c4820df83c558728f14914c19cfa16ffd2beee0
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-09 23:35:07 +0900

    added Item scriptable object

---
+++ unity/Assets/Scripts/Examples/ItemScriptableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Item
+{
+    [SerializeField]
+    string name;
+    public string Name { get { return name; } }
+
+    [SerializeField]
+    int price;
+    public int Price { get { return price; } }
+}
+
+[CreateAssetMenu]
+public class ItemScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    Item data;
+    public Item Data { get { return data; } }
+}
