50a4bef06465fa28570f35eab7e18c8a8eb10a18
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-10 19:38:31 +0900

    zip with index

--- unity/Assets/Scripts/Common/EnumerableEx.cs
+++ unity/Assets/Scripts/Common/EnumerableEx.cs
@@ -25,5 +25,29 @@ namespace System.Linq
                     yield return resultSelector(e1.Current, e2.Current);
                 }
         }
+
+        public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, int, TResult> resultSelector)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            return ZipImpl(first, second, resultSelector);
+        }
+
+        private static IEnumerable<TResult> ZipImpl<TFirst, TSecond, TResult>(
+            this IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second,
+            Func<TFirst, TSecond, int, TResult> resultSelector)
+        {
+            int counter = 0;
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    yield return resultSelector(e1.Current, e2.Current, counter++);
+                }
+        }
     }
 }
