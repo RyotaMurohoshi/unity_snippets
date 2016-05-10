8eab5676e38455048dd067f994f21276d6bd7c0c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-10 19:35:31 +0900

    addedd GenericScriptableObjects

---
+++ unity/Assets/Scripts/Examples/GenericScriptableObject/AbstractWeapon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AbstractWeapon : ScriptableObject
+{
+    [SerializeField]
+    int point;
+    public int Point { get { return point; } }
+
+    [SerializeField]
+    string weaponName;
+    public string WeaponName { get { return weaponName; } }
+}
