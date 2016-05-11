b9644be26e7763db34e66467cf4bc164003bc5e7
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-11 20:41:22 +0900

    put PlayerPrefs for Editor

---
+++ unity/Assets/Editor/DeletePlayerPrefsMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DeletePlayerPrefsMenu
+{
+    [MenuItem("Tools/Delete All PlayerPrefs")]
+    public static void DeleteAllPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
