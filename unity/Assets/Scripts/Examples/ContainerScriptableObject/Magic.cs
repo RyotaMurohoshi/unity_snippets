404f6eaea10aa173aaeface83f23f2034a5adaad
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-12 13:37:07 +0900

    created Container scriptable object

---
+++ unity/Assets/Scripts/Examples/ContainerScriptableObject/Magic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Magic
+{
+    [SerializeField]
+    string name;
+    public string Name { get { return name; } }
+}
---
+++ unity/Assets/Scripts/Examples/ContainerScriptableObject/MagicScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MagicScriptableObject : ScriptableObject, IContainer<Magic>
+{
+    [SerializeField]
+    Magic data;
+    public Magic Data { get { return data; } }
+}
