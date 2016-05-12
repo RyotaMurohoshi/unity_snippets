3aae6ea82416bbb4be4cab216c92251fb45df6c4
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-12 13:29:02 +0900

    created new yield

---
+++ unity/Assets/Scripts/Examples/YieldExample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YieldExample : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return ShowCoroutine("Hello", "World", "!");
+    }
+
+    IEnumerator ShowCoroutine(params string[] args)
+    {
+        foreach(var arg in args)
+        {
+            Debug.Log(arg);
+            yield return new WaitForSeconds(1.0F);
+        }
+    }
+}
