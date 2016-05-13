9fa7e42f6240a1b9f09b0cb1e7880bb8a51fc168
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-13 23:46:31 +0900

    added ResoueceLoader

---
+++ unity/Assets/Scripts/Examples/ContainerScriptableObject/ResourceLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ResourceLoader
+{
+    public static List<Magic> LoadMagicList()
+    {
+        return Resources.LoadAll<MagicScriptableObject>("ScriptableObjects/Magic").Select(it => it.Data).ToList();
+    }
+}
