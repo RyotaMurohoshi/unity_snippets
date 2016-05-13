2e44fdf391aa27962f02a3976bd89441b5da6cdb
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-13 23:48:42 +0900

    with generic

--- unity/Assets/Scripts/Examples/ContainerScriptableObject/ResourceLoader.cs
+++ unity/Assets/Scripts/Examples/ContainerScriptableObject/ResourceLoader.cs
@@ -6,6 +6,11 @@ public static class ResourceLoader
 {
     public static List<Magic> LoadMagicList()
     {
-        return Resources.LoadAll<MagicScriptableObject>("ScriptableObjects/Magic").Select(it => it.Data).ToList();
+        return LoadList<MagicScriptableObject, Magic>("ScriptableObjects/Magic");
+    }
+
+    public static List<TData> LoadList<TScriptableObject, TData>(string path) where TScriptableObject : Object, IContainer<TData>
+    {
+        return Resources.LoadAll<TScriptableObject>(path).Select(it => it.Data).ToList();
     }
 }
