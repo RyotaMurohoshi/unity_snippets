afd9c02b16528617cac81cfa8a413cc3c2c02e19
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-16 22:15:36 +0900

    created Builder

---
+++ unity/Assets/Editor/MenuItem/Builder.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+
+public static class Build
+{
+    [MenuItem("Tools/Build Production APK")]
+    public static void BuildApk()
+    {
+        var signConfigPath = Application.dataPath + "/../../SignConfig.json";
+        var signConfig = JsonUtility.FromJson<SignConfig>(File.ReadAllText(signConfigPath));
+
+        PlayerSettings.Android.keystoreName = signConfig.keystoreName;
+        PlayerSettings.Android.keystorePass = signConfig.keystorePass;
+        PlayerSettings.Android.keyaliasName = signConfig.keyaliasName;
+        PlayerSettings.Android.keyaliasPass = signConfig.keyaliasPass;
+
+        var outputPath = string.Format("{0}_{1}.apk", PlayerSettings.bundleIdentifier, PlayerSettings.bundleVersion);
+        var scenes = EditorBuildSettings
+            .scenes
+            .Where(it => it.enabled)
+            .ToArray();
+
+        string errorMessage = BuildPipeline.BuildPlayer(
+            levels: scenes,
+            locationPathName: outputPath,
+            target: BuildTarget.Android,
+            options: BuildOptions.None);
+
+        if (string.IsNullOrEmpty(errorMessage))
+        {
+            Debug.Log("[Success!]");
+        }
+        else {
+            Debug.LogError("[Error!] " + errorMessage);
+        }
+
+        PlayerSettings.Android.keystoreName = "";
+        PlayerSettings.Android.keystorePass = "";
+        PlayerSettings.Android.keyaliasName = "";
+        PlayerSettings.Android.keyaliasPass = "";
+    }
+}
+
+class SignConfig
+{
+    public string keystoreName;
+    public string keystorePass;
+    public string keyaliasName;
+    public string keyaliasPass;
+}
