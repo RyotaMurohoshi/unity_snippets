7545c4992b03b27bfdc2acc1bacdba71f70b0bb9
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-21 22:30:33 +0900

    added WaitForCompletion

---
+++ unity/Assets/Scripts/Examples/DoTween/DoTweenMoveCoroutineExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class DoTweenMoveCoroutineExample : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> targets;
+
+    [SerializeField]
+    Transform mover;
+
+    [SerializeField]
+    float velocity = 1.0F;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            foreach (var target in targets)
+            {
+                var distance = Vector3.Distance(target.position, mover.position);
+                var duration = distance / velocity;
+                yield return mover.transform.DOMove(target.position, duration).WaitForCompletion();
+            }
+        }
+    }
+}
