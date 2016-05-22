2a88e59a700fc2f2f07bb5eda614f17025493226
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-22 22:05:01 +0900

    canvas group tween

---
+++ unity/Assets/Scripts/Examples/DoTween/CanvasGroupTweenExample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class CanvasGroupTweenExample : MonoBehaviour
+{
+    [SerializeField]
+    CanvasGroup target;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return target.DOFade(0.0F, 2.0F).WaitForCompletion();
+            yield return new WaitForSeconds(1.0F);
+            yield return target.DOFade(1.0F, 2.0F).WaitForCompletion();
+        }
+    }
+}
