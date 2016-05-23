26f18d3d8702804ededd381f8e054575fc408713
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-23 17:52:42 +0900

    created Switcher

---
+++ unity/Assets/Scripts/Examples/Switcher.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections;
+
+public class Switcher : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform first;
+
+    [SerializeField]
+    RectTransform second;
+
+    IEnumerator Start()
+    {
+        var defaultPosition = first.transform.position;
+        var leftPosition = defaultPosition + Vector3.left * Screen.width;
+        var rightPosition = defaultPosition + Vector3.right * Screen.width;
+        var duration = 0.5F;
+
+        var current = first;
+        var next = second;
+        next.transform.position = rightPosition;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0F);
+
+            current.transform.DOMove(leftPosition, duration);
+            yield return next.transform.DOMove(defaultPosition, duration).WaitForCompletion();
+
+            var temp = current;
+            current = next;
+            next = temp;
+
+            next.transform.position = rightPosition;
+        }
+    }
+}
