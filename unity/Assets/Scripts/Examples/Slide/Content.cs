0829fd1415e8bb92a4dc48c27974a3f4768eec65
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-24 07:38:24 +0900

    created Content class
    moved directory

---
+++ unity/Assets/Scripts/Examples/Slide/Content.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[SerializeField]
+public class Content
+{
+    public Content(
+        string title,
+        string sentence,
+        Color textColor,
+        Color backgroundColor
+        )
+    {
+        this.title = title;
+        this.sentence = sentence;
+        this.textColor = textColor;
+        this.backgroundColor = backgroundColor;
+    }
+
+    [SerializeField]
+    string title;
+    public string Title { get { return title; } }
+
+    [SerializeField]
+    string sentence;
+    public string Sentence { get { return sentence; } }
+
+    [SerializeField]
+    Color textColor = Color.white;
+    public Color TextCollor { get { return textColor; } }
+
+    [SerializeField]
+    Color backgroundColor = Color.black;
+    public Color BackgroundColor { get { return backgroundColor; } }
+}
