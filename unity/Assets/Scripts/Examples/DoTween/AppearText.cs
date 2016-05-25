948f97671d08fa83fca0c56887417b47329062dd
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-05-25 23:08:25 +0900

    added apper text

---
+++ unity/Assets/Scripts/Examples/DoTween/AppearText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+[RequireComponent(typeof(Text))]
+public class AppearText : MonoBehaviour
+{
+    [SerializeField]
+    float duration = 0.5F;
+
+    [SerializeField]
+    float moveY = 100;
+
+    [SerializeField]
+    bool playOnStart;
+
+    void Start()
+    {
+        if (playOnStart)
+        {
+            Show();
+        }
+    }
+
+    public Tweener Show()
+    {
+        var originalPosition = transform.position;
+        transform.position += moveY * Vector3.down;
+        transform.DOMove(originalPosition, duration);
+
+        var text = GetComponent<Text>();
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0.0F);
+        return text.DOFade(1.0F, duration);
+    }
+}
