5ef45dbc5e107d02c98502c5fc215fa4823ebea7
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-07-04 22:35:07 +0900

    create cube

---
+++ unity/Assets/Scripts/Examples/TapReaction/TapReactionManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class TapReactionManager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnTap(Input.mousePosition);
+        }
+
+        if (Input.touchCount > 0)
+        {
+            foreach(var touch in Input.touches.Where(it => it.phase == TouchPhase.Began))
+            {
+                OnTap(touch.position);
+            }
+        }
+    }
+
+    void OnTap(Vector3 position)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = Camera.main.ScreenToWorldPoint(position);
+    }
+}
