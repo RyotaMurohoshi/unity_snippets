418fc346429b40aa013940745f035b251052bdda
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-07-16 22:25:04 +0900

    show assembly

---
+++ unity/Assets/Scripts/Common/ShowMethod.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Reflection;
+
+public class ShowMethod : MonoBehaviour
+{
+    void Start()
+    {
+        var assembly = Assembly.Load("UnityEngine.dll");
+        Debug.Log(assembly.FullName);
+    }
+}
