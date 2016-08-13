043b22a8065933ffe43c8b13d153342a94dc066d
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-13 22:10:54 +0900

    added method

--- unity/Assets/Scripts/Common/SocialMediaUtility.cs
+++ unity/Assets/Scripts/Common/SocialMediaUtility.cs
@@ -6,4 +6,9 @@ public static class SocialMediaUtility
     {
         Application.OpenURL(string.Format("market://details?id={0}", packageName));
     }
+
+    public static void OpenAppStore(string bundleIdentifier)
+    {
+        Application.OpenURL(string.Format("itms-apps://itunes.apple.com/app/id{0}?mt=8", bundleIdentifier));
+    }
 }
