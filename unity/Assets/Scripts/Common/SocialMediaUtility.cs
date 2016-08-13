fb3d62ce32fae8a261940f253b7ebf0cdb114e62
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-13 22:02:22 +0900

    added SocialMediaUtility

---
+++ unity/Assets/Scripts/Common/SocialMediaUtility.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class SocialMediaUtility
+{
+    public static void OpenGooglePlay(string packageName)
+    {
+        Application.OpenURL(string.Format("market://details?id={0}", packageName));
+    }
+}
