29ad770c0341f4e8fa425f475afe4589726be32c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-18 20:55:40 +0900

    created CountNumberText

---
+++ unity/Assets/Scripts/Examples/DoTween/CountNumberTextExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class CountNumberTextExample : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.0F);
+
+        yield return text
+            .DOTextInt(0, 100, it => string.Format("{0}!", 100 - it), 3.0F)
+            .SetEase(Ease.Linear)
+            .WaitForCompletion();
+
+        text.text = "Finish!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ unity/Assets/Scripts/Examples/DoTween/DoTweenEx.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using System;
+using DG.Tweening;
+
+public static class DOTweenEx
+{
+    public static Tweener DOTextInt(this Text text,int initialValue, int finalValue, Func<int, string> setter, float duration)
+    {
+        return DOTween.To(
+             () => initialValue,
+             it => text.text = setter(it),
+             finalValue,
+             duration
+         );
+    }
+
+    public static Tweener DOTextFloat(this Text text, float initialValue, float finalValue, Func<float, string> setter, float duration)
+    {
+        return DOTween.To(
+             () => initialValue,
+             it => text.text = setter(it),
+             finalValue,
+             duration
+         );
+    }
+
+    public static Tweener DOTextLong(this Text text, long initialValue, long finalValue, Func<long, string> setter, float duration)
+    {
+        return DOTween.To(
+             () => initialValue,
+             it => text.text = setter(it),
+             finalValue,
+             duration
+         );
+    }
+}
