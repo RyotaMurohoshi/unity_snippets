cbffedaee8d3d5340bc832cd7c68ebe0956cc805
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-20 22:10:16 +0900

    added WaitForCompletion Example

---
+++ unity/Assets/Scripts/Examples/DoTween/WaitForComplationExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class WaitForComplationExample : MonoBehaviour
+{
+    [SerializeField]
+    Transform mover;
+
+    [SerializeField]
+    List<Transform> positions;
+
+    IEnumerator Start()
+    {
+        foreach (var position in PositionRepeating(positions))
+        {
+            yield return mover
+                .DOMove(position, 0.5F)
+                .WaitForCompletion();
+        }
+    }
+
+    IEnumerable<Vector3> PositionRepeating(IEnumerable<Transform> positions)
+    {
+        while (true)
+        {
+            foreach (var it in positions)
+            {
+                yield return it.position;
+            }
+        }
+    }
+}
