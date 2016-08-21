136d1babbea08cc48a59af3a8eb09dce9b888c41
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-21 23:47:59 +0900

    created SimpleExample

---
+++ unity/Assets/Editor/Inspectors/PreviewSpriteDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PreviewSpriteAttribute))]
+public class PreviewSpriteDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        property.objectReferenceValue = EditorGUI.ObjectField(
+            rect,
+            label,
+            property.objectReferenceValue,
+            typeof(Sprite),
+            false);
+    }
+}
---
+++ unity/Assets/Scripts/Examples/Inspector/PreviewSpriteExample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreviewSpriteExample : MonoBehaviour
+{
+    [PreviewSprite]
+    [SerializeField]
+    Sprite sprite;
+}
