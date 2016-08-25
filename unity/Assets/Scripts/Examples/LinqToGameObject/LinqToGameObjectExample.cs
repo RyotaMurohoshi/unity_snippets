ba2cc81e876d1622fd1d50e5f4e6db2da3b112cc
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-26 00:05:06 +0900

    added example

---
+++ unity/Assets/Scripts/Examples/LinqToGameObject/LinqToGameObjectExample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Unity.Linq;
+using System.Linq;
+
+public class LinqToGameObjectExample : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+
+    void Start()
+    {
+        target.Children().Select(it => it.name);
+    }
+}
