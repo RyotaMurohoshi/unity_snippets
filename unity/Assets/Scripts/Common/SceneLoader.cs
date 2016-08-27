c1951c7e942ba3338b51c7d4472dae6b4ccf9074
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-08-27 23:24:49 +0900

    SceneLoader

---
+++ unity/Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+    {
+        SceneManager.LoadScene(sceneName, loadSceneMode);
+    }
+
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    public void LoadScene(int sceneBuildIndex, LoadSceneMode loadSceneMode)
+    {
+        SceneManager.LoadScene(sceneBuildIndex, loadSceneMode);
+    }
+}
+
