817c78fc1db9037591571aad56260f84900209aa
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-09-04 09:02:45 +0900

    added Example

--- unity_5_5/Assets/Scripts/Example.cs
+++ unity_5_5/Assets/Scripts/Example.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Example : MonoBehaviour
 {
+	public Rigidbody Rigidbody => GetComponent<Rigidbody>();
+	public string LoadName() => gameObject.name;
 }
