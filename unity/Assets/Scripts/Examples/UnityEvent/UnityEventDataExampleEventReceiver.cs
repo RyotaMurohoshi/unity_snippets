9792eb2d489cdc4f02c70fb00beb5c52b26c7a81
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-09-13 23:55:25 +0900

    moved ListExample

--- unity/Assets/Scripts/Examples/UnityEvent/UnityEventDataExampleEventReceiver.cs
+++ unity/Assets/Scripts/Examples/UnityEvent/UnityEventDataExampleEventReceiver.cs
@@ -2,7 +2,7 @@
 
 public class UnityEventDataExampleEventReceiver : MonoBehaviour
 {
-    public void Receive(Data data)
+    public void Receive(ColorData data)
     {
         Debug.Log(string.Format("{0} {1}", data.Name, data.Color));
     }
