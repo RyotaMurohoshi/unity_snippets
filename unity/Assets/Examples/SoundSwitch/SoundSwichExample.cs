bc591ae960d5956cb6dba07a102defeb68177abc
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-09-20 23:48:39 +0900

    added example

---
+++ unity/Assets/Examples/SoundSwitch/SoundSwichExample.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSwichExample : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+    [SerializeField]
+    Text text;
+
+    SoundSwitch soundSwitch;
+    bool enableSound;
+
+    void Awake()
+    {
+        soundSwitch = GetComponent<SoundSwitch>();
+        enableSound = false;
+        SetSoundEnable(enableSound);
+
+        button.onClick.AddListener(() =>
+        {
+            enableSound = !enableSound;
+            SetSoundEnable(enableSound);
+        });
+    }
+
+    void SetSoundEnable(bool enable)
+    {
+        text.text = enable ? "Sound Enable" : "Sound Disable";
+        if(enable)
+        {
+            soundSwitch.EnableSound();
+        }
+        else
+        {
+            soundSwitch.DisableSound();
+        }
+    }
+}
