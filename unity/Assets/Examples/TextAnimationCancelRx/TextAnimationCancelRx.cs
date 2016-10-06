2235d8cc240c1c06a0fcba38a7f57688a4c2df93
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-07 00:11:45 +0900

    update with UniRx

--- unity/Assets/Examples/TextAnimationCancelRx/TextAnimationCancelRx.cs
+++ unity/Assets/Examples/TextAnimationCancelRx/TextAnimationCancelRx.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections;
+using System;
+using UniRx;
 
 public class TextAnimationCancelRx : MonoBehaviour
 {
@@ -16,24 +18,34 @@ public class TextAnimationCancelRx : MonoBehaviour
     IEnumerator Start()
     {
         var textString = text.text;
-
-        button.onClick.AddListener(() =>
-        {
-            if (tweener != null)
-            {
-                tweener.Kill();
-            }
-        });
+        text.text = "";
 
         while (true)
         {
-            text.text = "";
-            this.tweener = text
-                .DOText(endValue: textString, duration: 10.0F)
+            tweener = text
+                .DOText(textString, 5)
                 .SetEase(Ease.Linear);
-            this.tweener.OnKill(() => text.text = textString);
-            yield return this.tweener.WaitForCompletion();
-            yield return new WaitForSeconds(1.0F);
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() =>
+            {
+                if (tweener != null)
+                {
+                    tweener.Kill();
+                    tweener = null;
+                    text.text = textString;
+                }
+            });
+
+            yield return tweener.WaitForCompletion();
+
+            yield return Observable.Merge(
+                Observable.Timer(TimeSpan.FromSeconds(1.0)).Select(_ => Unit.Default),
+                button.OnClickAsObservable()
+                )
+                .First()
+                .StartAsCoroutine();
+
+            text.text = string.Empty;
         }
     }
 }
