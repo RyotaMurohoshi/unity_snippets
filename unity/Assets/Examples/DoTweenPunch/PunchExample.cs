075429eefd1d0ae5f8e1c0bfe92a14ae65a12e86
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-14 00:08:24 +0900

    apply scene

---
+++ unity/Assets/Examples/DoTweenPunch/PunchExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PunchExample : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+
+    [SerializeField]
+    SpriteRenderer target;
+
+    void Awake()
+    {
+        button.onClick.AddListener(() => {
+            target.transform.DOPunchPosition(
+                punch : Vector3.up,
+                duration : 0.3F,
+                vibrato: 10,
+                elasticity : 1,
+                snapping: false);
+        });
+    }
+}
