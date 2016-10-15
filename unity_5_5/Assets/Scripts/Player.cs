c460b13bfb5a1a6b75caec4849ff78c7af10d002
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-15 19:17:01 +0900

    use nameof

--- unity_5_5/Assets/Scripts/Player.cs
+++ unity_5_5/Assets/Scripts/Player.cs
@@ -1,13 +1,13 @@
 ﻿public class Player
 {
-	public string Name { get; }
-	public int Level { get; }
+    public string Name { get; }
+    public int Level { get; }
 
-	public Player(string name, int level)
-	{
-		Name = name;
-		Level = level;
-	}
+    public Player(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
 
-	public override string ToString () => $"[name = {Name}, level = {Level}]";
+    public override string ToString() => $"{nameof(Player)}[name = {Name}, level = {Level}]";
 }
