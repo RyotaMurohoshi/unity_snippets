7ac41f1b2378cd7d6dd2858d56d1ee5019aa210b
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-17 23:59:28 +0900

    created example parts

---
+++ unity/Assets/Examples/DoTweenWaitForCompletion/ExampleMoveWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ExampleMoveWindow : MonoBehaviour
+{
+    const float Duration = 0.5F;
+
+    void Start()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + Screen.height);
+    }
+
+    public Tweener Show()
+    {
+        return transform.DOMove(Vector3.zero, Duration);
+    }
+
+    public Tweener Hide()
+    {
+        return transform.DOMove(Screen.height * Vector2.down, Duration);
+    }
+
+}
