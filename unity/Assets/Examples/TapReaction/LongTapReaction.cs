16ee4262f63bb3ed9098eed5c50503094e89d3af
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-19 21:49:20 +0900

    copied LongTapReaction

---
+++ unity/Assets/Examples/TapReaction/LongTapReaction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class LongTapReaction : MonoBehaviour
+{
+    public Coroutine Show()
+    {
+        return StartCoroutine(ShowCorutonine());
+    }
+
+    IEnumerator ShowCorutonine()
+    {
+        transform.localScale = Vector3.zero;
+        yield return transform.DOScale(1.0F * Vector3.one, 0.2F).WaitForCompletion();
+        Destroy(this.gameObject);
+    }
+}
