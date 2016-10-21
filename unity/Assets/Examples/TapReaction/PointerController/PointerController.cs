74ba78d1332da0f87ae36cab96523d4546c2dfbc
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-10-21 23:46:17 +0900

    created Controller

--- unity/Assets/Examples/TapReaction/PointerController/PointerController.cs
+++ unity/Assets/Examples/TapReaction/PointerController/PointerController.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UniRx;
+using UniRx.Triggers;
 
 public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     PointerReaction currentReaction;
+    Vector2ReactiveProperty vector2ReactiveProperty = new Vector2ReactiveProperty(Vector2.zero);
 
     [SerializeField]
     PointerReaction reactionPrefab;
+
+    [SerializeField]
+    GameObject target;
+
+    void Awake()
+    {
+        this.UpdateAsObservable()
+            .CombineLatest(vector2ReactiveProperty, (_, vec) => vec)
+            .Subscribe(it => {
+                target.transform.position += 0.025F * (Vector3)it;
+            });
+    }
 
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
@@ -17,6 +32,7 @@ public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpH
     {
         if (currentReaction != null)
         {
+            vector2ReactiveProperty.Value = Vector2.zero;
             currentReaction.Hide();
             currentReaction = null;
         }
@@ -26,10 +42,14 @@ public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpH
     {
         if (currentReaction != null)
         {
-            var position = Camera.main.ScreenToWorldPoint(eventData.position);
-            var distance = Vector3.Distance(currentReaction.transform.position, position);
+            var fromPosition = currentReaction.transform.position;
+            var toPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+            var distance = Vector3.Distance(fromPosition, toPosition);
             var scale = 2 * distance;
             currentReaction.ChangeScale(scale);
+
+            var diff = toPosition - fromPosition;
+            vector2ReactiveProperty.Value = Mathf.Min(5.0F, diff.magnitude) * diff.normalized;
         }
     }
 
@@ -40,5 +60,4 @@ public class PointerController : MonoBehaviour, IPointerDownHandler, IPointerUpH
         tapReaction.Show();
         return tapReaction;
     }
-
 }
