f6baa20038fc1d7888a028d5fbfd6213bbf34b30
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-11-06 16:31:35 +0900

    just copied

---
+++ unity_mono_update/Assets/Scripts/Example.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class Example : MonoBehaviour
+{
+    public Rigidbody Rigidbody => GetComponent<Rigidbody>();
+    public Collider Collider => GetComponent<Collider>();
+    public string GameObjectName() => gameObject.name;
+}
---
+++ unity_mono_update/Assets/Scripts/PlayerExample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerExample : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(new Player(name: "Ryota", level: 28));
+    }
+}
