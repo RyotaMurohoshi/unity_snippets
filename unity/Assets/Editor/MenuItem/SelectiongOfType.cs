3b2e129d44bf7d6d0c398493e32c6314398e226c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-11-22 23:33:34 +0900

    edit editor

---
+++ unity/Assets/Editor/MenuItem/SelectiongOfType.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class SelectiongOfType
+{
+    [MenuItem("Assets/Example/Selecting User Of Type/")]
+    public static void Show()
+    {
+        foreach (var texture in Selection.objects.OfType<Texture2D>())
+        {
+            Debug.Log(texture.name);
+        }
+    }
+}
