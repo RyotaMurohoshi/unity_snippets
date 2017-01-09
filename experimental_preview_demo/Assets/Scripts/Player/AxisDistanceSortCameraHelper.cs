3e1702422a1579416d6e3e7c118e412cfa8658a8
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2017-01-09 14:48:26 +0900

    import preview

---
+++ experimental_preview_demo/Assets/Scripts/Player/AxisDistanceSortCameraHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif        
+
+[ExecuteInEditMode]
+public class AxisDistanceSortCameraHelper : MonoBehaviour {
+	void Start () {
+        var camera = GetComponent<Camera>();
+        camera.transparencySortMode = TransparencySortMode.CustomAxis;
+        camera.transparencySortAxis = new Vector3(0.0f, 1.0f, -0.49f);
+        
+#if UNITY_EDITOR
+        foreach (SceneView sv in SceneView.sceneViews)
+        {
+            sv.camera.transparencySortMode = TransparencySortMode.CustomAxis;
+            sv.camera.transparencySortAxis = new Vector3(0.0f, 1.0f, -0.49f);
+        }
+#endif          
+	}
+}
